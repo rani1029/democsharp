770eda8d4d7b18ceb5e43537c34051d53ea39b5b
rani1029 <ranig1029@gmail.com>
2021-08-03 16:44:31 +0530

    added UC1

---
+++ EmployeeWageUc1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeWageUc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ranNum = new Random();
+            int present = ranNum.Next(0, 2);
+            Console.WriteLine("{present}");
+            if (present == 1)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+                Console.WriteLine("Employee is not present");
+
+
+
+        }
+    }
+}
